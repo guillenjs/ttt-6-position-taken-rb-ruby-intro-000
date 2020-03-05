8770cd07000d1372ba9a0217280de443a3d1bdf4
effective-cyborg-8832 <jorgeguilln@gmail.com>
2020-03-05 02:26:41 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
 if  board[0] == " " || board[0] == ''''
-  return false 
-else 
+  return false
+else
   return true
 end
 end
